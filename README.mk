842d2e2e6466e7f06ebfde63fb197cc4eb0a41c7
heming <heming@zto.com>
2020-10-31 22:54:16 +0800

    1.修改 README,补上对应的CSDN博客地址

--- README.mk
+++ README.mk
@@ -2,8 +2,8 @@
 可以在TestActivity内添加上需要测试的Host即可.
 主要核心就是如何去ping以及结果解析.
 可以适用于Android
-### 1.APP内网络域名测试.
-### 2.域名检测,主备域名切换.
+### APP内网络域名测试.
+### 域名检测,主备域名切换.
 
 具体应用场景可以参考我在CSDN写的博客.
->
+>[CSDN博客-Android主备域名切换实施方案](https://blog.csdn.net/heming9174/article/details/109384257)
